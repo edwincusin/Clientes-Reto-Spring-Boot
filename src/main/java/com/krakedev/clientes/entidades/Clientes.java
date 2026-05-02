3a5e09ebc1cf5058d74277179ae1daaf534ef3b1
EDWINCSN <c.edwin32@hotmail.com>
2026-05-02 10:51:05 -0500

    EGCA: creacion  PACKAGE ENTIDADES y clase Clientes

---
+++ src/main/java/com/krakedev/clientes/entidades/Clientes.java
@@ -0,0 +1,45 @@
+package com.krakedev.clientes.entidades;
+
+public class Clientes {
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	
+	public Clientes() {
+		//constructor vacio
+	}
+	//CONSTRUCTOR CON PARAMETROS
+	public Clientes(String cedula, String nombre, String apellido) {
+		super();
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+	//METODOS GET Y SET
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	//METODO SOBRE ESCCRITO DEL TO STRING 
+	@Override
+	public String toString() {
+		return "Clientes [cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + "]";
+	}
+	
+	
+	
+}
