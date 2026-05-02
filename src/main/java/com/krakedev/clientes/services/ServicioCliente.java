0d41b50237a2855e8569382133cbfa93a80804d1
EDWINCSN <c.edwin32@hotmail.com>
2026-05-02 11:55:48 -0500

    EGCA: implementacion de metodo actualizar y eliminar en la clase servicio cliente

--- src/main/java/com/krakedev/clientes/services/ServicioCliente.java
+++ src/main/java/com/krakedev/clientes/services/ServicioCliente.java
@@ -7,34 +7,55 @@ import com.krakedev.clientes.ClientesApplication;
 import com.krakedev.clientes.entidades.Cliente;
 
 public class ServicioCliente {
-	private ArrayList<Cliente> clientes= new ArrayList<Cliente>();
-	
-	
-	//METODO CLIENTE
+	private ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+
+	// METODO CLIENTE
 	public Cliente buscarporCedula(String cedulaString) {
-		
-		for(Cliente cliente:clientes) {
-			if(cliente.getCedula().equals(cedulaString)) {
+
+		for (Cliente cliente : clientes) {
+			if (cliente.getCedula().equals(cedulaString)) {
 				return cliente;
 			}
 		}
 		return null;
 	}
-	//METODO CREAR CLIENTE 
+
+	// METODO CREAR CLIENTE : retornamo cliente creado
 	public Cliente crear(Cliente clienteNuevo) {
-		Cliente existente=buscarporCedula(clienteNuevo.getCedula());
-		
-		if(existente==null) {
+		Cliente existente = buscarporCedula(clienteNuevo.getCedula());
+
+		if (existente == null) {
 			clientes.add(clienteNuevo);
 			return clienteNuevo;
-		}else {
-			return null;
 		}
+		return null;
+
+	}
+
+	// METODO LISTAR CLIENTES
+	public List<Cliente> listar() {
+		return clientes;
+	}
+
+	// METODO ACTUALIZAR : retornamos cliente actualizado
+	public Cliente actualizar(String cedula,Cliente clienteActualizado) {
+		Cliente cliente=buscarporCedula(cedula);	
+		if(cliente!=null) {
+			cliente.setNombre(clienteActualizado.getNombre());
+			cliente.setApellido(clienteActualizado.getApellido());
+		}
+		return cliente;
+	}
+	
+	//METODO ELIMINAR 
+	public boolean Eliminar(String cedula) {
+		Cliente clienteEliminar=buscarporCedula(cedula);
+		if (clienteEliminar!=null) {
+			return clientes.remove(clienteEliminar); // true
+		}
+		return false;
 		
 	}
 	
-	//METODO LISTAR CLIENTES
-	public List<Cliente> listar(){
-		return clientes;
-	}
+	
 }
